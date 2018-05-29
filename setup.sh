62d49090365edec0dbf746b769cfa7d59ac2151e
Paolococco94 <37830248+Paolococco94@users.noreply.github.com>
2018-05-30 00:28:44 +0200

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 cp AulaGamma_homework2.world ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/models/.
 cp turtlebot3_AulaGamma_homework2.launch ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/launch/.
 cp -r file.dae ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/models/.
-
+cp map/map.yaml ~/.
+cp map/map.pgm ~/.
 
 echo 
 echo HOMEWORK 2
@@ -17,19 +18,21 @@ echo
 echo Navigation
 echo
 echo Per poter procedere con la navigazione lanciare in un terminale: 
-echo     export TURTLEBOT3_MODEL=waffle
-echo     roslaunch turtlebot3_gazebo turtlebot3_AulaGamma_homework2.launch
 echo 
 echo Aprire un secondo terminale e digitare:
 echo     export TURTLEBOT3_MODEL=waffle
 echo     roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:=$HOME/map.yaml
-echo 
+echo
 echo Aprire un terzo terminale e digitare:
 echo     export TURTLEBOT3_MODEL=waffle
 echo     rosrun rviz rviz -d `rospack find turtlebot3_navigation`/rviz/turtlebot3_nav.rviz 
-echo
+echo    
 echo Usiamo il tasto 2D Pose Estimate per fornire la guess sulla posizione iniziale del robot
 echo Usiamo il tasto 2D Nav Goal per fornire la destinazione desiderata al robot
 echo
 echo
-
+echo Autonomous navigation
+echo
+echo Launch Turtlebot autonomous navigation:
+echo roslaunch turtlebot3_gazebo turtlebot3_simulation.launch
+echo
